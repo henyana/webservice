4db4fff0ecb9ac6021d3a7373e11b742a273f216
Hendrayana <henyana@gmail.com>
2022-08-04 01:18:59 +0700

    Update UserTableSeeder.php

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -19,35 +19,35 @@ class UserTableSeeder extends Seeder
         $user1->name = 'User 1';
         $user1->username = 'user1';
         $user1->email = 'user1@email.com';
-        $user1->password = 'user1pass';
+        $user1->password = Hash::make('user1pass');
         $user1->save();
 
         $user2 = new User;
         $user2->name = 'User 2';
         $user2->username = 'user2';
         $user2->email = 'user2@email.com';
-        $user2->password = 'user2pass';
+        $user2->password = Hash::make('user2pass');
         $user2->save();
 
         $user3 = new User;
         $user3->name = 'User 3';
         $user3->username = 'user3';
         $user3->email = 'user3@email.com';
-        $user3->password = 'user3pass';
+        $user3->password = Hash::make('user3pass');
         $user3->save();
 
         $user4 = new User;
         $user4->name = 'User 4';
         $user4->username = 'user4';
         $user4->email = 'user4@email.com';
-        $user4->password = 'user4pass';
+        $user4->password = Hash::make('user4pass');
         $user4->save();
 
         $user5 = new User;
         $user5->name = 'User 5';
         $user5->username = 'user5';
         $user5->email = 'user5@email.com';
-        $user5->password = 'user5pass';
+        $user5->password = Hash::make('user5pass');
         $user5->save();
     }
 }
